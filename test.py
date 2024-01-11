ef68bd3f3320c9585df2b522e986be31fda35d6c
HLorenTo97 <156368495+HLorenTo97@users.noreply.github.com>
2024-01-11 16:40:08 -0500

    Update test.py

--- test.py
+++ test.py
@@ -5,6 +5,8 @@ fecha_nacimiento = input("Introduce tu fecha de nacimiento (YYYY-MM-DD): ")
 # Calcula la edad a partir de la fecha de nacimiento
 fecha_nacimiento = datetime.datetime.strptime(fecha_nacimiento, '%Y-%m-%d')
 edad = datetime.datetime.now().year - fecha_nacimiento.year
+edad = datetime.datetime.now().year - fecha_nacimiento.year
+edad = datetime.datetime.now().year - fecha_nacimiento.year
 # Imprime la edad calculada
 
 print("Tu edad es:", edad)
